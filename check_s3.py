b9feb6ff48beb797c560750ca20fdbdfeb458440
mmendoza17 <marco.mendoza@wizeline.com>
2021-10-26 15:30:05 -0500

    Added function: Check if bucket already exist in S3

---
+++ check_s3.py
@@ -0,0 +1,34 @@
+"""
+S3 Sensor Connection Test
+"""
+
+from airflow import DAG
+from airflow.operators import SimpleHttpOperator, HttpSensor, BashOperator, EmailOperator, S3KeySensor
+from datetime import datetime, timedelta
+
+default_args = {
+    'owner': 'Marco Mendoza',
+    'depends_on_past': False,
+    'start_date': datetime(2016, 11, 1),
+    'retries': 1,
+    'retry_delay': timedelta(minutes=0)
+}
+
+dag = DAG('s3_dag_test', default_args=default_args, schedule_interval= '@once')
+
+t1 = BashOperator(
+    task_id='bash_test',
+    bash_command='echo "hello, it should work" > s3_conn_test.txt',
+    dag=dag)
+
+sensor = S3KeySensor(
+    task_id='check_s3_for_file_in_s3',
+    bucket_key='file-to-watch-*',
+    wildcard_match=True,
+    bucket_name='S3-Bucket-To-Watch',
+    s3_conn_id='my_conn_S3',
+    timeout=18*60*60,
+    poke_interval=120,
+    dag=dag)
+
+t1 >> sensor
